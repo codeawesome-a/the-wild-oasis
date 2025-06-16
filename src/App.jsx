c8ebc190fe2a772bfa94f19d4de0f378f633a502
codeawesome-a <abdellasiraje10@gmail.com>
2025-06-16 12:22:44 +0300

    creating the initial and checking the tailwind

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-green-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Tailwind is working without CLI! 🚀</h1>
+    </div>
+  );
+}
